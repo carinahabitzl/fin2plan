548a65976616bd1d19f51f32963bc1df7077b8da
LoboDeCastro <guilobcast@gmail.com>
2023-06-29 12:40:01 +0200

    Login-Validierung angelegt

--- jsf-jpa/src/main/java/at/campus02/bp2/mbean/LoginBean.java
+++ jsf-jpa/src/main/java/at/campus02/bp2/mbean/LoginBean.java
@@ -1,6 +1,10 @@
 package at.campus02.bp2.mbean;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 
 @ManagedBean
 public class LoginBean {
@@ -21,5 +25,22 @@ public class LoginBean {
 		this.password = password;
 	}
 	
-	
+	public void validateEmail(FacesContext context, 
+			UIComponent component,
+			Object value) throws ValidatorException {
+		
+		if (value == null) {
+			return;
+		}
+		
+		String regex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+		
+		if (!value.toString().matches(regex)) {
+			FacesMessage message = new FacesMessage("Ungültige E-Mail Adresse");
+			throw new ValidatorException(message);
+		}
+	}
 }
